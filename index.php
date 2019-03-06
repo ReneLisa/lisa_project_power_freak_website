788ac58a65f813b55a17bb891a7b86184b33c055
ReneLisa <renelisa11@gmail.com>
2019-03-06 17:21:14 +0100

    on avance

--- index.php
+++ index.php
@@ -7,7 +7,43 @@
     </head>
 
     <body>
-      Contenu
+      <header class="page-head">
+        <div class="container">
+
+          <div class="container-menu">
+            <div class="logo">
+              <img src="images/logo.png" alt="Logo Power Freak" width="auto" height="40vh"/>
+            </div>
+
+            <nav class="menu">
+              <ul>
+                <li><a class="nav-link active" href="./index.php">Le Recyclage</a></li>
+                <li><a class="nav-link" href="./deuxdex.php">A propos</a></li>
+                <li><a class="nav-link" href="./deuxdex.php">Contactez-nous</a></li>
+              </ul>
+            </nav>
+          </div>
+
+
+          <div class="container-icons">
+              <img src="images/facebook.png" class="icon" alt="Facebook"  width="auto" height="30vh" />
+              <img src="images/instagram.png" class="icon" alt="Instagram"  width="auto" height="30vh" />
+            </div>
+          </div>
+
+        </div>
+      </header>
+
+      <main class="page-content">
+        <div class="container">
+            <h1>Un an apres sa sortie, </br> POWER FREAK se fait un nouveau ECO-LOOK !</h1>
+            <div class="picture">
+              <img src="images/facebook.png" alt="" width="auto" height="30vh">
+            </div>
+        </div>
+      </main>
+
+
     </body>
 
 </html>
